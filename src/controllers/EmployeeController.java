c16adb99cbf6834a255586903c943255d55f36f0
matiaszapillon <matiaszapillon@gmail.com>
2018-10-27 00:07:14 -0300

    Agregado de controller Empleado y testeo de merge dev - master

---
+++ src/controllers/EmployeeController.java
@@ -0,0 +1,17 @@
+package controllers;
+import java.sql.SQLException;
+
+import data.*;
+import entities.Employee;
+
+public class EmployeeController {
+
+	private data.EmployeeData employeeData ;
+	
+	public EmployeeController() {
+		employeeData = new data.EmployeeData();		
+	}
+	
+	
+	
+}
